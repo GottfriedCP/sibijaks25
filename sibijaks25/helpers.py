ca2cd970df85eec87c34c55ea4935941514615ee
GP <gottfriedcpn@gmail.com>
2025-10-01 00:00:45 +0700

    logic tutup grr

--- sibijaks25/helpers.py
+++ sibijaks25/helpers.py
@@ -3,6 +3,6 @@ from django.http import Http404
 
 
 def _cek_deadline():
-    over_date = timezone.make_aware(timezone.datetime(2025, 10, 1, 0, 0, 0))
+    over_date = timezone.make_aware(timezone.datetime(2025, 10, 1, 1, 0, 0))
     if timezone.now() >= over_date:
         raise Http404("Kedaluwarsa")
