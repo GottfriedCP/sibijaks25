28403b04e896e047ef1ed5a9551085fe744330b1
GP <gottfriedcpn@gmail.com>
2025-11-12 00:20:10 +0700

    show ft peserta

--- sibijaks25/helpers.py
+++ sibijaks25/helpers.py
@@ -6,3 +6,10 @@ def _cek_deadline():
     over_date = timezone.make_aware(timezone.datetime(2025, 10, 1, 1, 0, 0))
     if timezone.now() >= over_date:
         raise Http404("Kedaluwarsa")
+
+
+def _cek_deadline_ft():
+    """Cek over date, tapi untuk full text."""
+    over_date = timezone.make_aware(timezone.datetime(2025, 11, 12, 1, 0, 0))
+    if timezone.now() >= over_date:
+        raise Http404("Kedaluwarsa")
